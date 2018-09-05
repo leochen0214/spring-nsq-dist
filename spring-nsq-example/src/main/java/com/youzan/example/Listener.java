09a868a66063bf0cf3c0fc5e7c1ce7fe877a491f
clong <chenlong@youzan.com>
2018-09-06 00:15:57 +0800

    add more documentation

--- spring-nsq-example/src/main/java/com/youzan/example/Listener.java
+++ spring-nsq-example/src/main/java/com/youzan/example/Listener.java
@@ -19,9 +19,9 @@ import java.util.List;
 public class Listener {
   private static final Logger logger = LoggerFactory.getLogger(Listener.class);
 
-//  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
+  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
   public void listener1(String json) {
-    logger.info("listener1 personStr={}", json);
+    logger.info("listener1 personJson={}", json);
   }
 
 
@@ -31,51 +31,47 @@ public class Listener {
   }
 
 
-  @NsqListener(topics = "JavaTesting-Finish", channel = "default")
-  public void listener3(List<Person> personList) {
-    logger.info("listener3 persons={}", personList);
+//  @NsqListener(topics = "JavaTesting-Finish", channel = "default")
+//  public void listener3(List<Person> personList) {
+//    logger.info("listener3 persons={}", personList);
+//  }
+
+  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
+  public void listener4(NSQMessage message) {
+    logger.info("listener4 message content={}, , headers={}", message.getReadableContent(), message.getJsonExtHeader());
+  }
+
+  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
+  public void listener5(NSQMessage message, Consumer consumer) {
+    logger.info("listener5 message content={}, , headers={}", message.getReadableContent(), message.getJsonExtHeader());
   }
 
 //  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
-//  public void listener4(NSQMessage message) {
-//    System.out.println(
-//        "listener4, message content=" + message.getReadableContent() + ", headers=" + message
-//            .getJsonExtHeader());
-//  }
-//
-//  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
-//  public void listener5(NSQMessage message, Consumer consumer) {
-//    System.out.println(
-//        "listener5, message content=" + message.getReadableContent() + ", headers=" + message
-//            .getJsonExtHeader());
-//  }
-//
-//  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
 //  public void listener6(Message<?> message) {
+//    logger.info("listener6 person={}", person);
 //    System.out.println(
 //        "listener6, message paylod=" + message.getPayload() + ", headers=" + message.getHeaders());
 //  }
-//
-//  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
-//  public void listener7(Message<Person> message) {
-//    System.out.println(
-//        "listener7, message paylod=" + message.getPayload() + ", headers=" + message.getHeaders());
-//  }
-//
-//  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
-//  public void listener8(@Payload Person message) {
-//    System.out.println("listener8, message paylod=" + message);
-//  }
 
-//  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
-  public void listener9(@Payload Person message, @Headers MessageHeaders headers) {
-    logger.info("listener9, message paylod={}, headers={}", message, headers);
+  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
+  public void listener6(Message<Person> message) {
+    logger.info("listener6 person={}, headers=", message.getPayload(), message.getHeaders());
   }
 
-//  @NsqListener(topics = "${topic}", channel = "default")
-//  public void listener10(@Payload Person message, @Headers Map<String, Object> headers) {
-//    System.out.println("listener10, message paylod=" + message + ", headers=" + headers);
-//  }
+  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
+  public void listener7(@Payload Person message) {
+    System.out.println("listener7, message paylod=" + message);
+  }
+
+  @NsqListener(topics = "JavaTesting-Ext", channel = "default")
+  public void listener8(@Payload Person message, @Headers MessageHeaders headers) {
+    logger.info("listener8, message paylod={}, headers={}", message, headers);
+  }
+
+  @NsqListener(topics = "${topic}", channel = "default")
+  public void listener9(@Payload Person message, @Headers Map<String, Object> headers) {
+    System.out.println("listener9, message paylod=" + message + ", headers=" + headers);
+  }
 
 
 }
